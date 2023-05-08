11412d2ea03bd2ae7087a5502588f312a6de432c
Edwin Cardenas <eacardenase@unal.edu.co>
2023-05-08 14:41:59 -0500

    feat: add product image entities and link it to typeorm

---
+++ src/products/entities/product-image.entity.ts
@@ -0,0 +1,12 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class ProductImage {
+  @PrimaryGeneratedColumn('uuid')
+  id: number;
+
+  @Column({
+    type: 'text',
+  })
+  url: string;
+}
