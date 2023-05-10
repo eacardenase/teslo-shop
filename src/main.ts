390da2cf2e32e4a11d01acf2e3b5435e8dd7ae4b
Edwin Cardenas <eacardenase@unal.edu.co>
2023-05-10 17:14:54 -0500

    feat: use nestjs built in logger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger('Bootstrap');
 
   app.setGlobalPrefix('api/v1');
 
@@ -16,5 +17,6 @@ async function bootstrap() {
   );
 
   await app.listen(process.env.PORT);
+  logger.log(`App running on port ${process.env.PORT}`);
 }
 bootstrap();
