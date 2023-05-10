a6a014b0647b848d279bd68024d060e38831e638
Edwin Cardenas <eacardenase@unal.edu.co>
2023-05-10 17:23:41 -0500

    feat: serve static public files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
+import { join } from 'path';
+
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 import { ProductsModule } from './products/products.module';
 import { CommonModule } from './common/common.module';
@@ -23,6 +26,10 @@ import { EnvConfiguration } from './config/app.config';
       password: process.env.DB_PASSWORD,
       autoLoadEntities: true,
       synchronize: true,
+    }),
+
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
     }),
 
     ProductsModule,
