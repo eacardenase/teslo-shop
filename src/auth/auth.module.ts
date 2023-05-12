9d4569736c0129c45fd147e23513ed52ec43f416
Edwin Cardenas <eacardenase@unal.edu.co>
2023-05-12 10:41:03 -0500

    feat: setup jwt for auth sync

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,14 +1,27 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PassportModule } from '@nestjs/passport';
 
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { User } from './entities/user.entity';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: {
+        expiresIn: '1d',
+      },
+    }),
+  ],
   exports: [TypeOrmModule],
 })
 export class AuthModule {}
--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -2,4 +2,5 @@ export const EnvConfiguration = () => ({
   environment: process.env.NODE_ENV || 'dev',
   port: process.env.PORT || 3030,
   hostAPI: process.env.HOST_API,
+  jwtSecret: process.env.JWT_SECRET,
 });
