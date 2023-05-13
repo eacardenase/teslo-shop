deca31bc1ee2cfd0d43c8395c0cc0db2beb9b35c
Edwin Cardenas <eacardenase@unal.edu.co>
2023-05-13 12:58:34 -0500

    feat: validate user role on protected routes

--- src/auth/guards/user-role/user-role.guard.ts
+++ src/auth/guards/user-role/user-role.guard.ts
@@ -1,6 +1,14 @@
 import { Reflector } from '@nestjs/core';
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
 import { Observable } from 'rxjs';
+
+import { User } from 'src/auth/entities/user.entity';
 
 @Injectable()
 export class UserRoleGuard implements CanActivate {
@@ -9,13 +17,32 @@ export class UserRoleGuard implements CanActivate {
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const user = request.user as User;
     const validRoles: string[] = this.reflector.get(
       'roles',
       context.getHandler(),
     );
 
-    console.log(validRoles);
+    if (!validRoles || validRoles.length === 0) {
+      return true;
+    }
 
-    return true;
+    if (!user) {
+      throw new BadRequestException('User not found');
+    }
+
+    for (const role of user.roles) {
+      if (validRoles.includes(role)) {
+        return true;
+      }
+    }
+
+    throw new ForbiddenException(
+      `User ${user.firstName} ${
+        user.lastName
+      } needs a valid role to access this resource.
+      Valid roles: ${validRoles.join(', ')}`,
+    );
   }
 }
